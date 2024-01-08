4d87c7e2f03031be5ff43455611c524db94e9f70
YJ9352 <act07612@gmail.com>
2024-01-08 17:28:33 +0900

    패키지 생성

---
+++ src/test/kotlin/com/teamsparta/reviewers/domain/exception/dto/ErrorResponse.kt
@@ -0,0 +1,4 @@
+package com.teamsparta.reviewers.domain.exception.dto
+
+class ErrorResponse {
+}
---
+++ src/test/kotlin/com/teamsparta/reviewers/domain/user/controller/UserController.kt
@@ -0,0 +1,4 @@
+package com.teamsparta.reviewers.domain.user.controller
+
+class UserController {
+}
