f99d77c7f0ef2b4b32922669b4b5c13d5e5fcfd7
doojoo9999 <parkbr95@naver.com>
2024-01-12 15:15:47 +0900

    기능 수정 - SecurityConfig 수정 ㄴ 오류 수정 완료 ㄴ package 누락 수정 ㄴ API 테스트 완료

--- src/main/kotlin/com/teamsparta/reviewers/domain/authorization/SecurityConfig.kt
+++ src/main/kotlin/com/teamsparta/reviewers/domain/authorization/SecurityConfig.kt
@@ -1,3 +1,4 @@
+package com.teamsparta.reviewers.domain.authorization
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
