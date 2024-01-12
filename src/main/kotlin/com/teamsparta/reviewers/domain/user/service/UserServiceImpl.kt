f965886437c64be5b1a444a646bbb020b858859f
YJ9352 <act07612@gmail.com>
2024-01-13 00:34:21 +0900

    토큰 유효성검사 수정

--- src/main/kotlin/com/teamsparta/reviewers/domain/user/service/UserServiceImpl.kt
+++ src/main/kotlin/com/teamsparta/reviewers/domain/user/service/UserServiceImpl.kt
@@ -48,8 +48,7 @@ class UserServiceImpl(
             ?: throw IllegalArgumentException("아이디 또는 비밀번호가 일치하지 않습니다.")
 
         val token = jwtTokenProvider.createToken(user.email)
-
-        if (token != jwtTokenProvider.validateToken(token)) {
+        if (jwtTokenProvider.validateToken(token) == null) {
             throw IllegalArgumentException("토큰이 유효하지 않습니다.")
         }
 
